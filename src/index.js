5e03feb354ed565079936dd7726edb8a8c10dda9
hueidou <hueidou163@gmail.com>
2024-07-03 16:12:03 +0800

    modify domain

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.201609.xyz": dockerHub,
+  "quay.201609.xyz": "https://quay.io",
+  "gcr.201609.xyz": "https://gcr.io",
+  "k8s-gcr.201609.xyz": "https://k8s.gcr.io",
+  "k8s.201609.xyz": "https://registry.k8s.io",
+  "ghcr.201609.xyz": "https://ghcr.io",
+  "cloudsmith.201609.xyz": "https://docker.cloudsmith.io",
+  "ecr.201609.xyz": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.201609.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
